d040f624a1645f75328d989ffd0f6f7d36f95e4f
fishope <831upsummer@gmail.com>
2019-03-25 17:20:33 +0800

    2-2

--- CounterHomework/app/src/main/java/com/example/counterhomework/MainActivity.java
+++ CounterHomework/app/src/main/java/com/example/counterhomework/MainActivity.java
@@ -1,16 +1,77 @@
 package com.example.counterhomework;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private int count=0;
+    private TextView show_count;
+    private static final String LOG_TAG ="test_perform";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        show_count=findViewById(R.id.show_count);
+
+        if(savedInstanceState!=null){
+            show_count.setText(savedInstanceState.getString("num_value"));
+            count=savedInstanceState.getInt("count");
+        }
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("num_value",show_count.getText().toString());
+        outState.putInt("count",count);
+        Log.d(LOG_TAG,"SAVEINSTANCE");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+    public void Count(View view) {
+        count++;
+        show_count.setText(Integer.toString(count));
+    }
 }
